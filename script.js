c4d7eaf7129d00b83003452ed24fe43f4aa4d942
Prachi <vaishyaprachi14@outlook.com>
2023-04-29 23:30:30 +0530

    li element updated

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addTask(){
     }
     else{
         let li = document.createElement("li");
+        li.classList.add("li-style");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
